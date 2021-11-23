b7a0840e1b0adad09d4e793b39d687ddc3b66d1b
leidychaconc <leidychaconc@gmail.com>
2021-11-23 16:51:22 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "google" {
     project = "strong-host-322803"
     region = "us-west1"
-    credentials = "/var/lib/jenkins/secrets.json"
+    credentials = "/var/lib/jenkins/secretsgoogle.json"
 }
