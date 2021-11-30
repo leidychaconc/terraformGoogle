cce48ca18454d5fdf65e6ee8e58c49bca31249cd
leidychaconc <leidychaconc@gmail.com>
2021-11-30 15:03:03 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -78,7 +78,7 @@ locals {
   rds_name = "odoo-${random_string.suffix.result}"
 }
 resource "random_string" "suffix" {
-  length  = 5
+  length  = 3
   special = false
   lower = true
   upper = false
