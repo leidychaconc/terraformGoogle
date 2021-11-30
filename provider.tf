889075d9184af2a201baee96ddc42ee9d2e1948d
leidychaconc <leidychaconc@gmail.com>
2021-11-30 10:54:15 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "google" {
     project = "weighty-utility-332922"
     region = "us-west1"
-    credentials = "/var/lib/jenkins/secretsgoogle.json"
+    credentials = "/var/lib/jenkins/secretsGoogle.json"
 }
